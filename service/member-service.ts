9ed846e629bf3653a72108d74c3984645290963b
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-06-02 00:39:27 +0200

    add search

--- service/member-service.ts
+++ service/member-service.ts
@@ -6,3 +6,16 @@ export const saveMember = async (member: Member, id: string) => {
   await setDoc(doc(firestore, 'members', id), member);
   console.log('Saved new member to firestore', member);
 };
+
+export const filterMembers = (members: Member[], query: string) => {
+  if (query.length === 0) {
+    return members;
+  }
+
+  const filteredMembers = members.filter((member) => {
+    console.log('member.memberDisplayId', member.memberDisplayId.toLowerCase());
+    console.log('query', query.toLowerCase());
+    return member.memberDisplayId.toLowerCase().includes(query.toLowerCase());
+  });
+  return filteredMembers;
+};
